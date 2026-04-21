2f22f1346e7758a1339e3662034ab377e817087e
Cameron Spencer <cameron.d.spencer3.mill@army.mil>
2026-04-21 15:56:34 -0500

    [Spencer/Egyinam] Built out LeaderService.java and created 1 passing test, shouldSaveNewLeader in LeaderServiceTest.java

--- src/test/java/com/team2pairing/api/services/LeaderServiceTest.java
+++ src/test/java/com/team2pairing/api/services/LeaderServiceTest.java
@@ -1,7 +1,44 @@
 package com.team2pairing.api.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.team2pairing.api.entity.Leader;
+import com.team2pairing.api.repository.LeaderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class LeaderServiceTest {
+
+    @Mock
+    LeaderRepository leaderRepository;
+
+    @InjectMocks
+    LeaderService leaderService;
+
+    @Test
+    void shouldSaveNewLeader() {
+        //Arrange
+        Leader leader = new Leader("Cameron", "Spencer", "CEO");
+        Leader savedLeader = new Leader("Cameron", "Spencer", "CEO");
+        savedLeader.setId(1L);
+
+        //Act
+        when(leaderRepository.save(leader)).thenReturn(savedLeader);
+        Leader result = leaderService.saveLeader(leader);
+
+        //Assert
+        assertThat(result.getId()).isEqualTo(1L);
+
+        verify(leaderRepository).save(leader);
+
+
+    }
 
 }
