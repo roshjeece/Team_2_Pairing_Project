733c575290dfafa7c051af3a36582d44ffa462d6
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-04-21 13:41:17 -0500

    Reece/Humphrey: getters and setters in Reviews, modified name to 'Review'

--- src/main/java/com/team2pairing/api/entity/Review.java
+++ src/main/java/com/team2pairing/api/entity/Review.java
@@ -7,8 +7,8 @@ import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name = "leader_reviews")
-public class Reviews {
+@Table(name = "leader_review")
+public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,7 +17,7 @@ public class Reviews {
     // Leader needs to be matched after merge
     @ManyToOne
     @JoinColumn(name = "leader_id", nullable = false)
-    private Leader leader;
+    private Leaders leader;
 
     @Column(name = "leader_rating", nullable = false)
     private Integer rating;
@@ -27,4 +27,18 @@ public class Reviews {
 
     @Column(name = "date_of_review", nullable = false)
     private LocalDate reviewDate;
+
+    public Review(Long id, Leaders leader, Integer rating, String description, LocalDate reviewDate) {
+        this.id = id;
+        this.leader = leader;
+        this.rating = rating;
+        this.description = description;
+        this.reviewDate = reviewDate;
+    }
+
+    public Review() {
+
+    }
+
+
 }
