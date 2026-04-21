1978d6d5a559d9ac05fb6646dbd190ee13c562fe
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-04-21 13:27:46 -0500

    Reece/Humphrey: entity Reviews added

---
+++ src/main/java/com/team2pairing/api/entity/Ratings.java
@@ -0,0 +1,4 @@
+package com.team2pairing.api.entity;
+
+public enum Ratings {
+}
--- src/main/java/com/team2pairing/api/entity/Reviews.java
+++ src/main/java/com/team2pairing/api/entity/Reviews.java
@@ -1,4 +1,30 @@
 package com.team2pairing.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "leader_reviews")
 public class Reviews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Leader needs to be matched after merge
+    @ManyToOne
+    @JoinColumn(name = "leader_id", nullable = false)
+    private Leader leader;
+
+    @Column(name = "leader_rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "review_description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "date_of_review", nullable = false)
+    private LocalDate reviewDate;
 }
