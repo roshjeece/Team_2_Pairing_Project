a427e2ae54917df2d10ba8b5d871ca850a3c704d
Cameron Spencer <cameron.d.spencer3.mill@army.mil>
2026-04-21 13:22:38 -0500

    [Spencer/Egyinam] Created Leader.java Entity, created directories for repository, service, and controller.

--- src/main/java/com/team2pairing/api/entity/Leaders.java
+++ src/main/java/com/team2pairing/api/entity/Leaders.java
@@ -1,4 +1,26 @@
 package com.team2pairing.api.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Leaders {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String fname;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
 }
