fe88f82255adc072190d84f8aa30c8d36ace6ead
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-04-22 14:43:32 -0500

    For Peter: Fixed Describe

---
+++ frontend/src/ratemyleader/review/__tests__/ReviewForm.test.tsx
@@ -0,0 +1,4 @@
+describe('Review Form', ()=>
+{
+
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'  // <- not 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [react()],
 })
